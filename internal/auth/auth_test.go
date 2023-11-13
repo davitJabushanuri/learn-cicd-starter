ccfc2e1806e6528c0e2af4f222c57b381838780b
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-11-13 17:49:20 +0400

    update run format check job

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -6,32 +6,32 @@ import (
 )
 
 func TestGetAPIKey(t *testing.T) {
-    t.Run("returns error when no Authorization header", func(t *testing.T) {
-        headers := http.Header{}
-        _, err := GetAPIKey(headers)
-        if err == nil {
-            t.Errorf("expected an error, got nil")
-        }
-    })
+	t.Run("returns error when no Authorization header", func(t *testing.T) {
+		headers := http.Header{}
+		_, err := GetAPIKey(headers)
+		if err == nil {
+			t.Errorf("expected an error, got nil")
+		}
+	})
 
-    t.Run("returns error when Authorization header is malformed", func(t *testing.T) {
-        headers := http.Header{}
-        headers.Add("Authorization", "malformed")
-        _, err := GetAPIKey(headers)
-        if err == nil {
-            t.Errorf("expected an error, got nil")
-        }
-    })
+	t.Run("returns error when Authorization header is malformed", func(t *testing.T) {
+		headers := http.Header{}
+		headers.Add("Authorization", "malformed")
+		_, err := GetAPIKey(headers)
+		if err == nil {
+			t.Errorf("expected an error, got nil")
+		}
+	})
 
-    t.Run("returns API key when Authorization header is well formed", func(t *testing.T) {
-        headers := http.Header{}
-        headers.Add("Authorization", "ApiKey 12345")
-        apiKey, err := GetAPIKey(headers)
-        if err != nil {
-            t.Errorf("expected no error, got %v", err)
-        }
-        if apiKey != "12345" {
-            t.Errorf("expected API key to be '12345', got '%v'", apiKey)
-        }
-    })
+	t.Run("returns API key when Authorization header is well formed", func(t *testing.T) {
+		headers := http.Header{}
+		headers.Add("Authorization", "ApiKey 12345")
+		apiKey, err := GetAPIKey(headers)
+		if err != nil {
+			t.Errorf("expected no error, got %v", err)
+		}
+		if apiKey != "12345" {
+			t.Errorf("expected API key to be '12345', got '%v'", apiKey)
+		}
+	})
 }
